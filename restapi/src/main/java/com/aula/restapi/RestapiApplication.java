9de8c5100af340daaf836f939cc322389699e684
Usuário 92 <feliperodrigoespindola@gmail.com>
2023-09-15 10:31:36 -0400

    atualização postgresql

--- restapi/src/main/java/com/aula/restapi/RestapiApplication.java
+++ restapi/src/main/java/com/aula/restapi/RestapiApplication.java
@@ -11,25 +11,24 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RestapiApplication {
 
-	public static void main(String[] args) throws SQLException {
-		SpringApplication.run(RestapiApplication.class, args);
-		Connection umaConexao = null;
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			umaConexao = DriverManager.getConnection("jdbc:mysql://localhost/contato", "user", "password");
-			ResultSet resultClient = umaConexao.createStatement().executeQuery("SELECT * FROM CLIENT");
-			while (resultClient.next()) {
-				System.out.println("Nome: " + resultClient.getString("nome"));
-			}
-		} catch (ClassNotFoundException notific) {
-			System.out.println("Driver do banco de dados não encontrado.");
-		}  catch (SQLException ex) {
-			System.out.println("Ocorreu um erro ao acessar o banco: " + ex.getMessage());
-		} finally {
-			if (umaConexao != null) {
-				umaConexao.close();
-			}
-		}
-	}
-
+    public static void main(String[] args) throws SQLException {
+        SpringApplication.run(RestapiApplication.class, args);
+        Connection umaConexao = null;
+        try {
+            Class.forName("org.postgresql.Driver");
+            umaConexao = DriverManager.getConnection("jdbc:postgresql://localhost:5432/contatos", "postgres", "123456");
+            ResultSet resultClient = umaConexao.createStatement().executeQuery("SELECT * FROM CLIENT");
+            while (resultClient.next()) {
+                System.out.println("Nome: " + resultClient.getString("nome"));
+            }
+        } catch (ClassNotFoundException notific) {
+            System.out.println("Driver do banco de dados não encontrado.");
+        } catch (SQLException ex) {
+            System.out.println("Ocorreu um erro ao acessar o banco: " + ex.getMessage());
+        } finally {
+            if (umaConexao != null) {
+                umaConexao.close();
+            }
+        }
+    }
 }
